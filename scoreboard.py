81a86b5071a5889e39df9396956e8c8a02ddee86
Wanice <52546308+wanicedude@users.noreply.github.com>
2022-11-21 06:43:28 +0100

    Renamed file main1.py to main.py

--- scoreboard.py
+++ scoreboard.py
@@ -7,20 +7,29 @@ class Score(Turtle):
         self.color("white")
         self.penup()
         self.score = 0
+        self.high_score = 0
         self.goto(x=0, y=270)
-        self.write(f"SCORE: {self.score}", True, align='center', font=('candara', 24, 'bold'))
+        # self.write(f"SCORE: {self.score}", True, align='center', font=('candara', 24, 'bold'))
         self.hideturtle()
+        self.update_scoreboard()
 
     def update_scoreboard(self):
         self.goto(x=0, y=270)
-        self.write(f"SCORE: {self.score}", True, align='center', font=('candara', 24, 'bold'))
+        self.clear()
+        self.write(f"SCORE: {self.score} High Score: {self.high_score}", True, align='center', font=('candara', 24, 'bold'))
 
     def scores(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
+        
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+        
 
-    def game_over(self):
-        self.goto(x=0, y=0)
-        self.write(f"Game Over", align='center', font=('candara', 24, 'bold'))
+    # def game_over(self):
+    #     self.goto(x=0, y=0)
+    #     self.write(f"Game Over", align='center', font=('candara', 24, 'bold'))
 
